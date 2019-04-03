32292103c1712356b23549074f4a386dc5b8b170
alex9302000 <ary9302000@gmail.com>
2019-04-02 21:46:09 -0400

    added the correct stuff?

---
+++ Game Jam 2/Assets/PopulationManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulationManager : MonoBehaviour
+{
+    public static int populationValue = 5;
+    public static int populationCapacity = 10;
+    public Text text;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = populationValue.ToString() + "/" + populationCapacity.ToString();
+    }
+    public static void ChangePopulation(int value)
+    {
+        populationValue += value;
+    }
+    public static void ChangePopulationCap(int value)
+    {
+        populationCapacity += value;
+    }
+
+}
---
+++ Game Jam 2/Assets/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static int resourceValue = 0;
+    public Text text;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = resourceValue.ToString();
+    }
+
+    public static void AddResource(int value)
+    {
+        resourceValue += value;
+    }
+
+    public static void RemoveResource(int value)
+    {
+        resourceValue -= value;
+    }
+}
